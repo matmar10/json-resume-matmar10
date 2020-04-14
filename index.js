0b276c47b84a74e9e3d42a95d9c4406c4c068b9c
Matthew J. Martin <matthew.mar10@gmail.com>
2020-04-14 15:54:45 +0700

    move files to public dir and add web server

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const publicDir = path.join(__dirname, 'public');
+const indexFile = path.join(publicDir, 'resume-flat.html');
+
+app.use(express.static(publicDir));
+app.get('/', (req, res) => res.sendFile(indexFile));
+
+const port = process.env.PORT || 9090;
+app.listen(port);
+console.log(`Listening on port ${port}`);
